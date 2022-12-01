946d010a3816e907f23b05b24081acdfa1c69115
yeiko25 <yeikosoto25@gmail.com>
2022-12-01 00:12:38 -0600

    Adiciones

---
+++ app/src/main/java/com/example/proyecto/model/User.kt
@@ -0,0 +1,21 @@
+package com.example.proyecto.model
+/*
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class User(
+    var id: String,
+    val nombre: String,
+    val apellido: String?,
+    val correo: String?,
+    val telefono: String?,
+    val usuario: String?,
+    val direccion: String?,
+    val clave: String?
+) : Parcelable{
+    constructor():
+            this("","","","","","","")
+}
+
+*/
---
+++ app/src/main/java/com/example/proyecto/repository/UserRepository.kt
@@ -0,0 +1,4 @@
+package com.example.proyecto.repository
+
+class UserRepository {
+}
